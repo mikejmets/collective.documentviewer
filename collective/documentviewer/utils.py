fc74ed90cfa77ec9088054ade28c10d4b5af196f
Mike Metcalfe <mike@webtide.co.za>
2015-05-17 12:03:22 +0200

    Added debug info

--- collective/documentviewer/utils.py
+++ collective/documentviewer/utils.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import errno
 from Products.CMFCore.utils import getToolByName
@@ -9,16 +10,32 @@ from collective.documentviewer.interfaces import IFileWrapper
 def getDocumentType(obj, allowed_types):
     ct = IFileWrapper(obj).file_type
     if ct is None:
+        logging.debug('getDocumentType: No File type for %s' % obj.id)
         return None
 
+    logging.debug('getDocumentType: File type %s for %s' % (
+        ct, obj.id))
     mime_registry = getToolByName(obj, 'mimetypes_registry')
     for _type in mime_registry.lookup(ct):
+        logging.debug('getDocumentType: lookup type %s for %s' % (
+         _type, obj.id))
         for ext in _type.extensions:
+            logging.debug('getDocumentType: lookup ext %s for %s' % (
+                ext, obj.id))
             if ext in EXTENSION_TO_ID_MAPPING:
                 id = EXTENSION_TO_ID_MAPPING[ext]
                 if id in allowed_types:
+                    logging.debug('getDocumentType: id %s FOUND for %s' % (
+                        id, obj.id))
                     return CONVERTABLE_TYPES[id]
+                else:
+                    logging.debug('getDocumentType: id %s NOT FOUND for %s' % (
+                        id, obj.id))
+            else:
+                logging.debug('getDocumentType: ext %s inot FOUND for %s' % (
+                    ext, obj.id))
 
+    logging.debug('getDocumentType: not FOUND for %s' % (obj.id))
     return None
 
 
